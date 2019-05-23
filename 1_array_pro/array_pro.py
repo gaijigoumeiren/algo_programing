c51fb5d8a5e2cbc6281d8b9f87526ee1abfe4c00
gaijigoumeiren <464838956@qq.com>
2019-05-23 23:19:51 +0800

    修正3sum closest的bug

--- 1_array_pro/array_pro.py
+++ 1_array_pro/array_pro.py
@@ -42,6 +42,67 @@ def three_sum_closest_test():
     print(c)
 
 
+def fourSum_v1(nums, target):
+    """
+    这是我第一版的fourSum，速度超慢，39%
+    :param nums:
+    :param target:
+    :return:
+    """
+    n = len(nums)
+    if n == 0:
+        return []
+    result = []
+    nums.sort()
+    # 加上这行判定后，速度从964 ms降到172ms，超过62%
+    if 4 * nums[0] > target or 4 * nums[-1] < target:
+        return []
+    def three_sum(nums, start, end, target):
+        result = []
+        # 加上这行判定后，速度从964 ms降到172ms，超过62%
+        if 3 * nums[start] > target or 3 * nums[end] < target:
+            return []
+        for i in range(start, end - 1):
+            if i > start and nums[i] == nums[i - 1]:
+                continue
+            t_target = target - nums[i]
+            right = end
+            left = i + 1
+            while True:
+                if left >= right:
+                    break
+                tmp = t_target - nums[right] - nums[left]
+                if tmp == 0:
+                    result.append([nums[i], nums[right], nums[left]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    right -= 1
+                elif tmp > 0:
+                    left += 1
+                else:
+                    right -= 1
+        return result
+
+    for i in range(0, n - 3):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        t_target = target - nums[i]
+        three_s = three_sum(nums, i + 1, n - 1, t_target)
+        for th in three_s:
+            th.append(nums[i])
+            result.append(th)
+
+    return result
+
+
+def four_sum_test():
+    a = [1, 0, -1, 0, -2, 2]
+    rr = fourSum(a, 0)
+    print(rr)
 
 if __name__ == '__main__':
-    three_sum_closest_test()
+    # three_sum_closest_test()
+    four_sum_test()
